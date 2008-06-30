29687502a62e1a712ba9238515737a639ebc7e8f
kfukuda <kfukuda>
2008-06-30 11:34:48 +0000

    [232282] refactoring flash part

--- plugins/org.eclipse.actf.visualization.flash/src/org/eclipse/actf/visualization/flash/ui/properties/FlashNodePropertySource.java
+++ plugins/org.eclipse.actf.visualization.flash/src/org/eclipse/actf/visualization/flash/ui/properties/FlashNodePropertySource.java
@@ -25,68 +25,38 @@ import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
 
-public class FlashNodePropertySource implements IPropertySource {
+public class FlashNodePropertySource implements IPropertySource, IFlashConst {
 
 	private static boolean DEBUG_MODE = false;
 
 	private ASNode flashNode;
-	private boolean isAccImpl;
 
-	public static final String PID_OBJECTNAME = "OBJECTNAME", //$NON-NLS-1$
-			PID_TYPE = "TYPE", //$NON-NLS-1$
-			PID_VALUE = "VALUE", //$NON-NLS-1$
-			PID_TARGET = "TARGET", //$NON-NLS-1$
-			PID_DEPTH = "depth", //$NON-NLS-1$
-			PID_CURRENTFRAME = "currentFrame", //$NON-NLS-1$
-			PID_CLASSNAME = "CLASSNAME", //$NON-NLS-1$
-			PID_TEXT = "TEXT", //$NON-NLS-1$
-			PID_TITLE = "TITLE", //$NON-NLS-1$
-			PID_UICOMPONENT = "isUIComponent", //$NON-NLS-1$
-			PID_OPAQUE_OBJECT = "isOpaqueObject", //$NON-NLS-1$
-			PID_INPUTABLE = "isInputable", //$NON-NLS-1$
-			PID_LOCATION = "LOCATION", //$NON-NLS-1$
-			PID_ACC_ROLE = "ACC_ROLE", //$NON-NLS-1$
-			PID_ACC_NAME = "ACC_NAME", //$NON-NLS-1$
-			PID_ACC_STATE = "ACC_STATE", //$NON-NLS-1$
-			PID_ACC_ACTION = "ACC_ACTION", //$NON-NLS-1$
-			PID_ACC_INFO = "ACC_INFO", //$NON-NLS-1$
-			PID_TABINDEX = "tabIndex", //$NON-NLS-1$
-			PID_WMODE = "WMODE"; //$NON-NLS-1$
+	public static final String PID_LOCATION = "Location", //$NON-NLS-1$
+			PID_TABINDEX = "tabIndex"; //$NON-NLS-1$
 
 	private static final IPropertyDescriptor[] DESCRIPTORS = new IPropertyDescriptor[] {
-			new PropertyDescriptor(PID_OBJECTNAME, "objectName"), // 0
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_TYPE, "type"), // 1 //$NON-NLS-1$
-			new PropertyDescriptor(PID_VALUE, "value"), // 2 //$NON-NLS-1$
-			new PropertyDescriptor(PID_TARGET, "target"), // 3 //$NON-NLS-1$
-			new PropertyDescriptor(PID_DEPTH, PID_DEPTH),
-			new PropertyDescriptor(PID_CURRENTFRAME, PID_CURRENTFRAME),
-			new PropertyDescriptor(PID_CLASSNAME, "className"), // 6
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_TEXT, "text"), // 7 //$NON-NLS-1$
-			new PropertyDescriptor(PID_TITLE, "title"), // 8 //$NON-NLS-1$
-			new PropertyDescriptor(PID_LOCATION, "Location"), // 10
-			// //$NON-NLS-1$
+			new PropertyDescriptor(ASNODE_OBJECT_NAME, ASNODE_OBJECT_NAME), // 0
+			new PropertyDescriptor(ASNODE_TYPE, ASNODE_TYPE), // 1
+			new PropertyDescriptor(ASNODE_VALUE, ASNODE_VALUE), // 2
+			new PropertyDescriptor(ASNODE_TARGET, ASNODE_TARGET), // 3
+			new PropertyDescriptor(ASNODE_DEPTH, ASNODE_DEPTH),
+			new PropertyDescriptor(ASNODE_CURRENT_FRAME, ASNODE_CURRENT_FRAME),
+			new PropertyDescriptor(ASNODE_CLASS_NAME, ASNODE_CLASS_NAME), // 6
+			new PropertyDescriptor(ASNODE_TEXT, ASNODE_TEXT), // 7
+			new PropertyDescriptor(ASNODE_TITLE, ASNODE_TITLE), // 8
+			new PropertyDescriptor(PID_LOCATION, PID_LOCATION), // 10
 			new PropertyDescriptor(PID_TABINDEX, PID_TABINDEX),
-			new PropertyDescriptor(PID_ACC_INFO, "accInfo"), // 11
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_ACC_ROLE, "accRole"), // 11
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_ACC_NAME, "accName"), // 12
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_ACC_STATE, "accState"), // 13
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_ACC_ACTION, "accDefaultAction"), // 14
-			// //$NON-NLS-1$
-			new PropertyDescriptor(PID_UICOMPONENT, PID_UICOMPONENT),
-			new PropertyDescriptor(PID_OPAQUE_OBJECT, PID_OPAQUE_OBJECT),
-			new PropertyDescriptor(PID_INPUTABLE, PID_INPUTABLE),
-			new PropertyDescriptor(PID_WMODE, "wmode") // 15 //$NON-NLS-1$
+			new PropertyDescriptor(ASNODE_ACCINFO, ASNODE_ACCINFO), // 11
+			new PropertyDescriptor(ASNODE_IS_UI_COMPONENT,
+					ASNODE_IS_UI_COMPONENT),
+			new PropertyDescriptor(ASNODE_IS_OPAQUE_OBJECT,
+					ASNODE_IS_OPAQUE_OBJECT),
+			new PropertyDescriptor(ASNODE_IS_INPUTABLE, ASNODE_IS_INPUTABLE),
+			new PropertyDescriptor(ATTR_WMODE, ATTR_WMODE) // 15 //$NON-NLS-1$
 	};
 
 	public FlashNodePropertySource(ASNode flashNode) {
 		this.flashNode = flashNode;
-		this.isAccImpl = "_accImpl".equals(flashNode.getObjectName()); //$NON-NLS-1$
 	}
 
 	public Object getEditableValue() {
@@ -113,52 +83,42 @@ public class FlashNodePropertySource implements IPropertySource {
 
 	public Object getPropertyValue(Object id) {
 		String strValue = null;
-		if (PID_OBJECTNAME.equals(id)) {
+		if (ASNODE_OBJECT_NAME.equals(id)) {
 			strValue = flashNode.getObjectName();
-		} else if (PID_TYPE.equals(id)) {
+		} else if (ASNODE_TYPE.equals(id)) {
 			strValue = flashNode.getType();
-		} else if (PID_VALUE.equals(id)) {
+		} else if (ASNODE_VALUE.equals(id)) {
 			strValue = flashNode.getValue();
-		} else if (PID_TARGET.equals(id)) {
+		} else if (ASNODE_TARGET.equals(id)) {
 			strValue = flashNode.getTarget();
 			// strValue += ", mName="+flashNode.getString("mName") + ",
 			// mTarget="+flashNode.getString("mTarget") + ",
 			// parentTarget="+flashNode.getString("parentTarget") + ",
 			// targetIsString="+flashNode.getString("targetIsString") + ",
 			// targetModified="+flashNode.getString("targetModified");
-		} else if (PID_CLASSNAME.equals(id)) {
+		} else if (ASNODE_CLASS_NAME.equals(id)) {
 			strValue = flashNode.getClassName();
-		} else if (PID_TEXT.equals(id)) {
+		} else if (ASNODE_TEXT.equals(id)) {
 			strValue = flashNode.getText(false);
-		} else if (PID_TITLE.equals(id)) {
+		} else if (ASNODE_TITLE.equals(id)) {
 			strValue = flashNode.getTitle();
-		}
-		// else if( PID_DEPTH.equals(id) ) {
-		// strValue = flashNode.getString("depth"); //$NON-NLS-1$
-		// }
-		// else if( PID_CURRENTFRAME.equals(id) ) {
-		// strValue = flashNode.getString("currentFrame"); //$NON-NLS-1$
-		// }
-		// else if( PID_UICOMPONENT.equals(id) ) {
-		// strValue = flashNode.getString("isUIComponent"); //$NON-NLS-1$
-		// }
-		else if (PID_LOCATION.equals(id)) {
+		} else if (PID_LOCATION.equals(id)) {
 			try {
 				double x = flashNode.getX(); //$NON-NLS-1$
 				double y = flashNode.getY(); //$NON-NLS-1$
 				double w = flashNode.getWidth(); //$NON-NLS-1$
 				double h = flashNode.getHeight(); //$NON-NLS-1$
-				if (Double.NaN == x) {
+				if (Double.NaN != x) {
 					strValue = x
 							+ ", " + y + ", " + (x + w) + ", " + (y + h) + " (width=" + w + ", height=" + h + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
 				}
 			} catch (Exception e) {
 			}
-		} else if (PID_WMODE.equals(id)) {
+		} else if (ATTR_WMODE.equals(id)) {
 			if (null == flashNode.getParent()) {
 				strValue = flashNode.getPlayer().getWMode();
 			}
-		} else if (PID_ACC_INFO.equals(id)) {
+		} else if (ASNODE_ACCINFO.equals(id)) {
 			ASAccInfo accInfo = flashNode.getAccInfo(); //$NON-NLS-1$
 			if (null != accInfo) {
 				AttributePropertySource attrSource = new AttributePropertySource(
@@ -178,40 +138,12 @@ public class FlashNodePropertySource implements IPropertySource {
 				}
 				return attrSource;
 			}
-		} else if (isAccImpl) {
-			String methodName = null;
-			if (PID_ACC_ROLE.equals(id)) {
-				methodName = "get_accRole"; //$NON-NLS-1$
-			} else if (PID_ACC_NAME.equals(id)) {
-				methodName = "get_accName"; //$NON-NLS-1$
-			} else if (PID_ACC_STATE.equals(id)) {
-				methodName = "get_accState"; //$NON-NLS-1$
-			} else if (PID_ACC_ACTION.equals(id)) {
-				methodName = "get_accDefaultAction"; //$NON-NLS-1$
-			}
-			if (null != methodName) {
-				Object value = flashNode.getPlayer().callMethod(
-						flashNode.getTarget(), methodName); //$NON-NLS-1$
-
-				if (null != value) {
-					if (value instanceof String) {
-						strValue = (String) value;
-					} else if (value instanceof Integer) {
-						int intValue = (Integer) value;
-						if (PID_ACC_ROLE.equals(id)) {
-							strValue = MSAA.getRoleText(intValue);
-						} else if (PID_ACC_STATE.equals(id)) {
-							strValue = MSAA.getStateText(intValue);
-						}
-					}
-				}
-			}
 		} else if (null == strValue) {
 			Object obj = flashNode.getObject(id.toString());
 			if (null != obj) {
 				strValue = obj.toString();
 			}
-		}		
+		}
 		return strValue;
 	}
 
