d1623bed3ae5b9ecb928cbc570d31fc90f948a6d
kfukuda <kfukuda>
2009-03-12 09:00:23 +0000

    [260231] integration into Eclipse platform

--- plugins/org.eclipse.actf.visualization.blind.html/src/org/eclipse/actf/visualization/internal/blind/html/ui/HTMLPerspective.java
+++ plugins/org.eclipse.actf.visualization.blind.html/src/org/eclipse/actf/visualization/internal/blind/html/ui/HTMLPerspective.java
@@ -13,16 +13,53 @@
 package org.eclipse.actf.visualization.internal.blind.html.ui;
 
 import org.eclipse.actf.visualization.ui.IVisualizationPerspective;
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.views.IViewRegistry;
 
-public class HTMLPerspective implements IPerspectiveFactory, IVisualizationPerspective {
+@SuppressWarnings("nls")
+public class HTMLPerspective implements IPerspectiveFactory,
+		IVisualizationPerspective {
+
+	private static final String RESOURCE_FOLDER = "actf.html.resource.folder";
+
+	private static final String RESOURCE_NAVIGATOR = "org.eclipse.ui.views.ResourceNavigator";
+	private static final String PACKAGE_EXPLORER = "org.eclipse.jdt.ui.PackageExplorer";
 
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(true);
-		
-		//layout info: moved to fragment XML
-		//initializer: moved to Startup.java
-	}	
-	
+
+		// layout info: moved to fragment XML
+		// initializer: moved to Startup.java
+
+		String editorArea = layout.getEditorArea();
+
+		// check existence of Resource Navigator/Package Explorer
+		// (do not add place holder in the case of RCP)
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		boolean hasResourceNavigator = false;
+		boolean hasPackageExplorer = false;
+		if (workbench != null) {
+			IViewRegistry viewReg = workbench.getViewRegistry();
+			hasResourceNavigator = (viewReg.find(RESOURCE_NAVIGATOR) != null);
+			hasPackageExplorer = (viewReg.find(PACKAGE_EXPLORER) != null);
+		}
+
+		if (hasResourceNavigator) {
+			IFolderLayout resourceFolder = layout.createFolder(RESOURCE_FOLDER,
+					IPageLayout.LEFT, 0.2f, editorArea);
+			resourceFolder.addView(RESOURCE_NAVIGATOR);
+			if (hasPackageExplorer) {
+				resourceFolder.addPlaceholder(PACKAGE_EXPLORER);
+			}
+		} else if (hasPackageExplorer) {
+			IFolderLayout resourceFolder = layout.createFolder(RESOURCE_FOLDER,
+					IPageLayout.LEFT, 0.2f, editorArea);
+			resourceFolder.addView(PACKAGE_EXPLORER);
+		}
+	}
+
 }
