bdafaff0c341e1a65aadb565686221311e87ad22
kfukuda <kfukuda>
2009-06-15 08:01:21 +0000

    [260230] Documentation for Visualization components

--- plugins/org.eclipse.actf.visualization.presentation/src/org/eclipse/actf/visualization/presentation/IPresentationVisualizationModes.java
+++ plugins/org.eclipse.actf.visualization.presentation/src/org/eclipse/actf/visualization/presentation/IPresentationVisualizationModes.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and Others
+ * Copyright (c) 2008, 2009 IBM Corporation and Others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,9 @@ package org.eclipse.actf.visualization.presentation;
 
 import org.eclipse.actf.visualization.presentation.util.ParamRoom;
 
+/**
+ * Interface for specifying mode of presentation visualization.
+ */
 public interface IPresentationVisualizationModes {
 
 	/**
