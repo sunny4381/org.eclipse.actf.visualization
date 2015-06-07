d9b227c45341529dce537ce4fbbcc12524794db7
Kentarou Fukuda <kentarou@jp.ibm.com>
2015-06-08 02:05:11 +0900

    Bug 466220 - False-positive in Blind analisys (API doc correction)

--- plugins/org.eclipse.actf.visualization.eval/src/org/eclipse/actf/visualization/eval/html/HtmlTagUtil.java
+++ plugins/org.eclipse.actf.visualization.eval/src/org/eclipse/actf/visualization/eval/html/HtmlTagUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and Others
+ * Copyright (c) 2004, 2015 IBM Corporation and Others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -95,7 +95,7 @@ public class HtmlTagUtil implements IHtmlEventHandlerAttributes {
 	 *            target {@link Node}
 	 * @param ancestorName
 	 *            target ancestor tag name
-	 * @return true if target {@link Node} has target ancestor
+	 * @return target ancestor {@link Node} or null
 	 */
 	public static Node getAncestor(Node target, String ancestorName) {
 		Node tmpNode = target;
